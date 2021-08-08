4864375b0f6e3b8078d53a6201daa7b9108c3a26
Amitoj Singh <35400192+amitojsingh366@users.noreply.github.com>
2021-08-08 06:23:35 +0300

    chore: commit

--- packages/website/src/components/VideoPlayer.tsx
+++ packages/website/src/components/VideoPlayer.tsx
@@ -88,13 +88,8 @@ export const VideoPlayer: FC<IframeProps> = ({
     }, [(wrapper.connection ? wrapper.connection.authed : wrapper.connection), player, roomUser]);
 
     return (
-        <div ref={ref} className="plyr__video-embed" id="player">
-            <iframe
-                src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
-                allowFullScreen
-                allowTransparency
-                allow="autoplay"
-            ></iframe>
-        </div>
+       
+           <div ref={ref} id="player" data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+        
     )
 }
