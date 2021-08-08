c7c623e6baec809ae8759d99aa6f2ecac24603b1
Amitoj Singh <amitojsingh366@gmail.com>
2021-08-08 04:16:52 +0300

    feat(website): add video

---
+++ packages/website/src/components/VideoPlayer.tsx
@@ -0,0 +1,99 @@
+import { DetailedHTMLProps, FC, IframeHTMLAttributes, useEffect, useState } from "react"
+import Plyr from "plyr";
+import 'plyr/dist/plyr.css';
+import { RoomUser } from "@bunnytube/wrapper";
+import { useWrappedConn } from "../hooks/useConn";
+
+export type IframeProps = DetailedHTMLProps<
+    IframeHTMLAttributes<HTMLIFrameElement>,
+    HTMLIFrameElement
+> & {
+    roomUser: RoomUser | undefined
+};
+
+export const VideoPlayer: FC<IframeProps> = ({
+    roomUser,
+}) => {
+    const wrapper = useWrappedConn();
+    const ytRegex = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+    const [player, setPlayer] = useState<Plyr>()
+    let lastStatus = 0;
+
+    const sendStatus = () => {
+        if (!player) return;
+
+        let match = typeof player.source === "string" ? (player.source as string).match(ytRegex) : undefined;
+        wrapper.mutation.playback.sendStatus({
+            source: 'YOUTUBE',
+            isPlaying: player.playing,
+            paused: player.paused,
+            videoUrl: match ? match[1] : undefined,
+            currentTime: player.currentTime ? player.currentTime : 0
+        });
+    }
+
+    const sendRateLimitedStatus = () => {
+        if (Date.now() - lastStatus < 2500) return;
+        lastStatus = Date.now();
+        sendStatus();
+    }
+
+    useEffect(() => {
+        if (!player) setPlayer(new Plyr('#player'));
+        if (!wrapper.connection || !player) return;
+        wrapper.subscribe.playback.status((status) => {
+            if (status.data.isPlaying) {
+                if (!player.playing && !player.paused) {
+                    if (status.data.source == "YOUTUBE") player.source = {
+                        type: 'video',
+                        sources: [{
+                            src: status.data.videoUrl ? `https://www.youtube.com/embed/${status.data.videoUrl}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1` : "",
+                            provider: 'youtube'
+                        }]
+                    }
+                    player.play();
+                }
+                let currentSourceMatch = (player.source as unknown as string).match(ytRegex);
+                if (currentSourceMatch && currentSourceMatch[1] !== status.data.videoUrl) {
+                    if (status.data.source == "YOUTUBE") player.source = {
+                        type: 'video',
+                        sources: [{
+                            src: status.data.videoUrl ? `https://www.youtube.com/embed/${status.data.videoUrl}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1` : "",
+                            provider: 'youtube'
+                        }]
+                    }
+                    player.play();
+                }
+                if ((status.data.currentTime - 3 > player.currentTime) || (status.data.currentTime + 3 < player.currentTime)) player.currentTime = status.data.currentTime;
+                if (player.paused && !status.data.paused) player.play();
+            }
+            if (status.data.paused) player.pause();
+        })
+
+    }, [wrapper.connection, player]);
+
+    useEffect(() => {
+        if (!wrapper.connection || !wrapper.connection.authed || !player || !roomUser) return;
+        player.on('play', sendStatus)
+        player.on('pause', sendStatus);
+        player.on('seeked', sendRateLimitedStatus);
+
+        setInterval(() => {
+            if (roomUser && (roomUser.role == "ADMINISTRATOR" || roomUser.role == "MODERATOR")) {
+                sendStatus();
+            }
+        }, 3000);
+
+    }, [(wrapper.connection ? wrapper.connection.authed : wrapper.connection), player, roomUser]);
+
+    return (
+        <div className="plyr__video-embed" id="player">
+            <iframe
+                src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+                allowFullScreen
+                allowTransparency
+                allow="autoplay"
+            ></iframe>
+        </div>
+    )
+}
--- packages/website/src/pages/room/[id].tsx
+++ packages/website/src/pages/room/[id].tsx
@@ -5,21 +5,36 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { RoomUser } from "@bunnytube/wrapper";
 import { v4 as uuidv4 } from "uuid";
+import { VideoPlayer } from "../../components/VideoPlayer";
 
 export default function Home() {
     const wrapper = useWrappedConn();
     const { query, replace } = useRouter();
     const roomId = typeof query.id === "string" ? query.id : "";
+    const [roomUser, setRoomUser] = useState<RoomUser>();
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState<Array<{ author: RoomUser, content: string, id: string }>>([]);
     const [loading, setLoading] = useState(false);
+    const ytRegex = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
 
     const sendMessage = async () => {
         if (message !== "") {
             setLoading(true);
+            let match = message.match(ytRegex);
+            if (match) {
+                const resp = await wrapper.mutation.playback.sendStatus({
+                    videoUrl: match ? match[1] : "",
+                    paused: false,
+                    source: 'YOUTUBE',
+                    currentTime: 0,
+                    isPlaying: true
+                })
+                if (resp.success) { setMessage(""); setLoading(false); return; }
+            }
             const resp = await wrapper.mutation.chat.sendMessage(message);
             setLoading(false);
             if (resp.success) setMessages((current) => current.concat(resp.message));
+            setMessage("");
         }
     }
 
@@ -27,7 +42,8 @@ export default function Home() {
         if (!wrapper.connection || !wrapper.connection.authed) return;
 
         wrapper.mutation.room.join(roomId).then((resp) => {
-            if (!resp.success) replace("/dash")
+            if (!resp.success) replace("/dash");
+            if (resp.success) setRoomUser(resp.roomUser);
         })
 
         wrapper.subscribe.chat.message((message) => {
@@ -82,8 +98,9 @@ export default function Home() {
     return (
         <div className="w-full h-full">
             <div className="flex flex-col content-center justify-center p-10 gap-2">
+                <VideoPlayer roomUser={roomUser} />
                 {messages && messages.map((m) => <p key={m.id}>{m.author.username}: {m.content}</p>)}
-                <Input type="text" placeholder="Message" onChange={(e) => setMessage(e.target.value)} />
+                <Input type="text" placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
                 <Button loading={loading} onClick={sendMessage}>Send Message</Button>
             </div>
         </div>
